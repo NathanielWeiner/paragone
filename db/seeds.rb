30fe415e8d98f0ddd381379ac0657bbdc0e8f6ee
michaeltien <michaeltien@users.noreply.github.com>
2015-02-07 22:15:52 -0500

    push

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ abram = Curation.find_or_create_by!( title: "Abram's Curations", description: "H
 quint = Curation.find_or_create_by!( title: "Quint's Curations", description: "His better favorite list of art")
 
 ids = [15642, 10416, 492, 6997, 27564, 20010, 10681, 37626, 4432, 30663]
+quints = [26419, 27282, 16689, 11819, 31455, 6997, 16751, 36492, 14623]
 
 artwork_objects = []
 
@@ -22,4 +23,17 @@ artwork_objects.each do |object|
   num = object['ObjectNumber']
   art_object = ArtObject.create_with(object_number: num).find_or_create_by!(id: id)
   abram.art_objects << art_object
+end
+
+artwork_objects = []
+
+quints.each do |id|
+  artwork_objects << TheWalters::ArtObject.find(id)
+end
+
+artwork_objects.each do |object|
+  id = object['ObjectID']
+  num = object['ObjectNumber']
+  art_object = ArtObject.create_with(object_number: num).find_or_create_by!(id: id)
+  quint.art_objects << art_object
 end
